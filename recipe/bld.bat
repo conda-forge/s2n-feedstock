1d1427783c97cc97d5f69d56254119e25565bdae
David Brochart <david.brochart@gmail.com>
2020-11-17 04:08:43 +0100

    Build static lib too

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-mkdir "%SRC_DIR%"\build
-pushd "%SRC_DIR%"\build
+mkdir build-shared
+cd build-shared
 
 cmake -G "Ninja" ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
@@ -7,6 +7,22 @@ cmake -G "Ninja" ^
       -DCMAKE_INSTALL_LIBDIR=lib ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DBUILD_SHARED_LIBS=ON ^
+      ..
+if errorlevel 1 exit 1
+
+ninja install
+if errorlevel 1 exit 1
+cd ..
+
+mkdir build-static
+cd build-static
+
+cmake -G "Ninja" ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      -DCMAKE_INSTALL_LIBDIR=lib ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DBUILD_SHARED_LIBS=OFF ^
       ..
 if errorlevel 1 exit 1
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-mkdir build
-pushd build
+mkdir build-shared
+cd build-shared
 cmake ${CMAKE_ARGS} -GNinja \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
@@ -13,4 +13,16 @@ cmake ${CMAKE_ARGS} -GNinja \
   -DBUILD_SHARED_LIBS=ON \
   ..
 ninja install
-popd
+cd ..
+
+mkdir build-static
+cd build-static
+cmake ${CMAKE_ARGS} -GNinja \
+  -DCMAKE_PREFIX_PATH=$PREFIX \
+  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+  -DCMAKE_INSTALL_LIBDIR=lib \
+  -DENABLE_TESTING=OFF \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  ..
+ninja install
