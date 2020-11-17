97e88b5f74e9e1bb885d04e8ef783a4a53729479
David Brochart <david.brochart@gmail.com>
2020-11-17 14:13:23 +0100

    Create outputs for shared and static libs

---
+++ recipe/install.sh
@@ -0,0 +1,11 @@
+if [[ "$PKG_NAME" == *static ]]
+then
+    cd build-static
+    ninja install
+    # relying on conda to dedup package
+    echo "Keeping all files, conda will dedupe"
+else
+    cd build-shared
+    ninja install
+    rm -rf ${PREFIX}/lib/*.a
+fi
